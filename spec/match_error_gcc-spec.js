377de5f0ec2ecef4449232e3b8a5a4c1bb200b3d
Morten V. Pedersen <morten@mortenvp.com>
2016-07-03 21:12:06 +0200

    Remove commented code

--- spec/match_error_gcc-spec.js
+++ spec/match_error_gcc-spec.js
@@ -8,21 +8,6 @@ describe("Match errors from gcc", () => {
 
         let output = fs.readFileSync(`${__dirname}/error_gcc.txt`, 'utf-8')
 
-/*        expect(matchError(output)).toEqual([
-            {file: '/lib-core/test/src/test_select_type.cpp',
-             line: '31',
-             col: '71',
-             message: ' type/value mismatch ‘template<class BaseType, class DefaultType> struct kodo_core::select_type’'},
-            {file: '/lib-core/src/lib_core/select_symbol_id_writer_layers.hpp',
-             line: '35',
-             col: '9',
-             message: ' expected nested-name-specifier before ‘select_type2’'},
-            {file: '/lib-core/src/lib_core/full_vector_decoder.hpp',
-             line: '19',
-             col: '45',
-             message: ' kodo_core/select_storage_type.hpp: No such file or directory'}]
-        );*/
-
         expect(matchError(output)).toEqual([
             new Error('/lib-core/test/src/test_select_type.cpp',
                       '31', '71',
